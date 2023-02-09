3ad35bf8b3fbc382bc9bf1d9bfaefdbce64ba244
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2023-02-09 14:00:40 +0700

    clear all

--- expo-sb/App.tsx
+++ expo-sb/App.tsx
@@ -1,17 +1,12 @@
-import { store } from './app/store';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
-import { Main } from 'screens';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Main/>
-      </View>
-    </Provider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
