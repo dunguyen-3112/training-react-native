2ff75f52b649b7971042c8f009707515182d8c4b
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2023-02-13 13:59:11 +0700

    #2 add config alias import and test app

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,14 @@
+import { Test } from '@components';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Test />
     </View>
   );
 }
