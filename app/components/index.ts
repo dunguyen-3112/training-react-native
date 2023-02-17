36ebe6de02e4bcf6ea261b8aeb3f106eca1cdda6
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2023-02-17 17:07:50 +0700

    Define Constant Color

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,7 +1,10 @@
-import Tag from './Tag';
 import Food from './Food';
 import Foods from './Foods';
 import CustomText from './CustomText';
 import Search from './Search';
+import Icon from './Icon';
+import Tags from './Tags';
+import Slide from './Slide';
+import Back from './Back';
 
-export { Tag, Food, Foods, CustomText, Search };
+export { Tags, Food, Foods, CustomText, Search, Icon, Slide, Back };
