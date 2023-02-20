fccb9189c4d799a9319d3353ce7f685351375ca3
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2023-02-20 15:13:40 +0700

    - Implement Not Found Favorite #11 - Implement Empty Search #10

---
+++ app/components/Empty.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
+import CustomText from './CustomText';
+
+const Empty = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('@assets/images/empty.png')} />
+      <CustomText
+        text="No Results Found"
+        weight="500"
+        size={22}
+        marginTop={24}
+        color="#696969"
+      />
+      <CustomText color="#7e7e7e" size={13} marginTop={8}>
+        Try search for a different keywork or
+      </CustomText>
+      <CustomText color="#7e7e7e" size={13}>
+        tweek your search a little
+      </CustomText>
+    </View>
+  );
+};
+
+export default Empty;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingTop: 122,
+  },
+});
