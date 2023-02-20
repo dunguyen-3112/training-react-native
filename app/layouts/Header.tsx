4f42c772b438275418f12487fbf7a7d1d128b689
dunguyen-3112 <dunguyenhuu.it2019@gmail.com>
2023-02-20 15:17:00 +0700

    Update Header layout

--- app/layouts/Header.tsx
+++ app/layouts/Header.tsx
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import CustomText from '../components/CustomText';
-import HelpButton from '../components/HelpButton';
+import { CustomButton } from '@components';
 
 const Header = () => {
   return (
@@ -10,7 +10,14 @@ const Header = () => {
         <CustomText text="Want to eat" color="black" size={23} weight="800" />
         <CustomText text="healthy Food?" color="black" size={23} weight="800" />
       </View>
-      <HelpButton />
+      <CustomButton
+        width={35}
+        height={35}
+        borderRadius={35 / 2}
+        backgroundColor="rgba(0,0,0,0.1)"
+      >
+        <Image source={require('@assets/icons/question.png')} />
+      </CustomButton>
     </View>
   );
 };
